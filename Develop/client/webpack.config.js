b1d4e6bdd6d03d0dead22dfec473bc528198d414
Jason Grant <jason82autotech@gmail.com>
2024-01-09 12:18:00 -0800

    completed todos in webpack.config.js file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,28 +3,48 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
-
-    module: {
-      rules: [
-        
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new WebpackPwaManifest({
+      name: 'My App',
+      short_name: 'App',
+      description: 'My awesome app',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'sw.js',
+    }),
+  ],
 };
+
