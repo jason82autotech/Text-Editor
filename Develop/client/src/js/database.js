0f95df28e129d42b31ba29ea890cfb2ade8448a5
Jason Grant <jason82autotech@gmail.com>
2024-01-09 12:12:44 -0800

    todo's in database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,14 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Add your implementation logic here
+  console.log('putDb implementation');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Add your implementation logic here
+  console.log('getDb implementation');
+};
 
 initdb();
